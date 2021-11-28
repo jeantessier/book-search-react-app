85e8f7640445bca931e787327fe04b49c3143682
Jean Tessier <jean@jeantessier.com>
2021-11-27 22:17:19 -0800

    Use search query typed in search box

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function SearchBox({ q, search }) {
     return (
         <div>
-            <input type="text" name="q" value={q}/>
-            <button onClick={() => search("foobar")}>Search</button>
+            <input type="text" name="q" value={q} onChange={e => search(e.target.value)}/>
         </div>
     )
 }
