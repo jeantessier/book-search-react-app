199056ca05ab4b2396078465cc0652850701a4f7
Jean Tessier <jean@jeantessier.com>
2025-11-17 12:14:22 -0800

    Tidy up unnecessary import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
